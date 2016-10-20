35e6d9d54d7c9e5ffb4039f934d3fbf6fdd18ff4
laramie14 <laramie.martinez@gmail.com>
2016-10-19 21:46:29 -0600

    Add files via upload
    Added Base random key generators, encryption and compression methods and generations of the MD5 has and sha256.key files.

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+#include <Wincrypt.h>
 #include sha256.key
+
+#define BUFSIZE 1024
+#define MD5LEN  16
 int main (int argc,  char** argv)
 {
 printf("Drop File Here.");
 return 0;
-FILE *input;
-input = fopen(argv[1], "wb+");
+FILE *fopen( const char * /sha256.key, const char * w+ );
 if(sha256.key) {
 /*If true then program will ask to decrypt; checks for internal sha256.key*/
 printf("File is encrypted,would you like to decrypt it?\n ");
@@ -14,5 +19,127 @@ else {
 	printf("File isn't encrypted, would you like to encrypt it?\n");
 }
 /*Encrypting code*/
+/*Generate random key A*/
 
+
+char *mkrndstr(size_t length) { // const size_t length, supra
+
+static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!"; // could be const
+char *randomString;
+
+if (length) {
+    randomString = malloc(length +1); // sizeof(char) == 1, cf. C99
+
+    if (randomString) {
+        int l = (int) (sizeof(charset) -1); // (static/global, could be const or #define SZ, would be even better)
+        int key;  // one-time instantiation (static/global would be even better)
+        for (int n = 0;n < length;n++) {        
+            key = rand() % l;   // no instantiation, just assignment, no overhead from sizeof
+            randomString[n] = charset[key];
+        }
+
+        randomString[length] = '\0';
+    }
 }
+
+return randomString;
+}
+return 0;
+
+/*Return file with Key A*/
+/*Generate random key B*/
+char *mkrndstr(size_t length) { // const size_t length, supra
+
+static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!"; // could be const
+char *randomString;
+
+if (length) {
+    randomString = malloc(length +1); // sizeof(char) == 1, cf. C99
+
+    if (randomString) {
+        int l = (int) (sizeof(charset) -1); // (static/global, could be const or #define SZ, would be even better)
+        int key;  // one-time instantiation (static/global would be even better)
+        for (int n = 0;n < length;n++) {        
+            key = rand() % l;   // no instantiation, just assignment, no overhead from sizeof
+            randomString[n] = charset[key];
+        }
+
+        randomString[length] = '\0';
+    }
+}
+
+return randomString;
+}
+return 0;
+
+/*Generate MD5*/
+
+from wincrypto import CryptCreateHash, CryptHashData, CryptDeriveKey, CryptEncrypt, CryptImportKey, CryptExportKey
+from wincrypto.constants import CALG_SHA1, CALG_AES_256, bType_SIMPLEBLOB
+
+#derive key from password
+sha1_hasher = CryptCreateHash(CALG_SHA1)
+CryptHashData(sha1_hasher, 'Password')
+aes_key = CryptDeriveKey(sha1_hasher, CALG_AES_256)
+
+#encrypt data using key
+encrypted_data = CryptEncrypt(aes_key, 'secret data')
+
+#Import a PUBLICKEYBLOB and export the AES key as SIMPLEBLOB
+TEST_RSA_PUBLIC_MSKEYBLOB = '0602000000a40000525341310004000001000100d1537575000617a37093cec958e8adedd347b5812f' \
+                            '702595fc02fbb870f6a17a26780f9147a6cd938dffff842a1427f8200621f822caaf9b338b4bb3dbda' \
+                            'ce58eedfc7b29b91a1f5ce628657f30f3feb9d909a1a00bd484f628f2db38087eec2f6bb4df1df024b' \
+                            '19af1c97d316c86073c972059d65bc2b47f97e5462a2e8029a'.decode('hex')              
+rsa_pub_key = CryptImportKey(TEST_RSA_PUBLIC_MSKEYBLOB)
+encrypted_aes_key = CryptExportKey(aes_key, rsa_pub_key, bType_SIMPLEBLOB)
+
+return 0;
+
+/*Generate sha256.key File */
+
+int Crypto (int argc, char** argv)
+{
+    char calc_hash[65];
+
+    calc_sha256("file.file", calc_hash);
+}
+
+int calc_sha256 (char* path, char output[65])
+{
+    FILE* file = fopen(path, "rb");
+    if(!file) return -1;
+
+    char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    const int bufSize = 32768;
+    char* buffer = malloc(bufSize);
+    int bytesRead = 0;
+    if(!buffer) return -1;
+    while((bytesRead = fread(buffer, 1, bufSize, file)))
+    {
+        SHA256_Update(&sha256, buffer, bytesRead);
+    }
+    SHA256_Final(hash, &sha256);
+
+    sha256_hash_string(hash, output);
+    fclose(file);
+    free(buffer);
+    return 0;
+}      
+
+void sha256_hash_string (char hash[SHA256_DIGEST_LENGTH], char outputBuffer[65])
+{
+    int i = 0;
+
+    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+
+    outputBuffer[64] = 0;
+}
+return 0;
+int End (void);
+printf("Project Flashpoint encrypted.");
+return 0;
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+from tests.algorithms import *
+from tests.native import *
+
+if __name__ == '__main__':
+    unittest.main()
